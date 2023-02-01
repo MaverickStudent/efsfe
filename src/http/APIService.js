530c597cd1f58cad4a19f634ef8365f9f393bb86
MaverickStudent <miguelrocha@unomaha.edu>
2023-02-01 16:33:39 -0700

    Update APIService.js

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 import axios from 'axios';
 
-// const API_URL = 'https://maverickstudent1.pythonanywhere.com/';
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://maverickstudent1.pythonanywhere.com/';
+// const API_URL = 'http://127.0.0.1:8000';
 
 export class APIService {
     constructor() {}
